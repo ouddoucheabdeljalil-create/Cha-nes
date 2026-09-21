6b742b6f3c16b70d87c1451ba419c0feeeec05fb
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-21 16:38:47 +0200

    Inversion de Chaîne

--- challange5.js
+++ challange5.js
@@ -1,7 +1,8 @@
 const prompt = require('prompt-sync')();
-const text = "abcd"
-const x = []
+const text = "YOUCODE" ;
+const x = [] ;
 for (let i = text.length-1 ; i >= 0 ; i--){
     x.push(text[i]);
 }
-console.log(x.join(""))
+console.log(`Before : ${text}`) ;
+console.log("After : " ,x.join("")) ;
