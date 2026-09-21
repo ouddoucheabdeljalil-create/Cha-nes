d3e89d761f49e43ead7dd86a65962adf771c0aa1
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-21 14:36:19 +0200

    Concaténation de Chaînes

--- challange3.js
+++ challange3.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
-const text1 = ["enter","le","nombre"];
-const text2 = ["de" , " numero"];
+const text1 = ["enter" ,"le","nombre"];
+const text2 = ["de","numero"];
 const text3 = [];
 
-text3.push(text1,text2)
-console.log(text3)
+text3.push(text1,text2);
+console.log(text3.join(" "));
